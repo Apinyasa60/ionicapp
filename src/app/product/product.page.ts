73a5de9301d49a30078e6bc6232a8bf1a808ce79
Apinya Saison <Apinyasaison9@gmail.com>
2020-01-17 11:45:07 +0700

    1.0.0

---
+++ src/app/product/product.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.page.html',
+  styleUrls: ['./product.page.scss'],
+})
+export class ProductPage implements OnInit {
+
+  titleName:string;
+  productName:string;
+  buttonName:string;
+ 
+  constructor(public alertController: AlertController) { }
+
+  ngOnInit() {
+    this.titleName = 'รายการอาหาร'
+    this.productName = 'ข้าวผัดกระเพาหมูหมัก ไข่ดาว'
+    this.buttonName = 'ราคา'
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'ข้าวผัด',
+      subHeader: 'ราคาสินค้า',
+      message: 'ราคา 50 บาท',
+      buttons: ['OK']
+    })
+    await alert.present();
+  }
+  async cancelAlert() {
+      const alert = await this.alertController.create({
+        message: 'ยกเลิกรายการอาหาร',
+        buttons: ['OK']
+      })
+    await alert.present();
+  }
+}
