23ca1697fe4ca53d7a779831a43ec95ad20ecacf
Apinya Saison <Apinyasaison9@gmail.com>
2020-03-06 12:04:38 +0700

    1.0.10

--- src/app/customer/customer.page.ts
+++ src/app/customer/customer.page.ts
@@ -7,19 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerPage implements OnInit {
 
-  id: string;
-  name: string;
-  lastname: string;
-  address: string;
-  tel: string
-
-  input_id: string;
-  input_name: string;
-  input_lastname: string;
-  input_address: string;
-  input_tel: string;
-
-
   constructor() { }
 
   ngOnInit() {
